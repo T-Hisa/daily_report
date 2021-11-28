2bf005495f8dc7548f8c08f1ddac58e06c831750
Glitch (hello-express) <none>
2021-11-28 08:13:59 +0000

    👘💵 Checkpoint ./app.js:39260817/2229 ./app_mention.js:39260817/665 ./app_home_opened.js:39260817/1406

--- app_mention.js
+++ app_mention.js
@@ -1,1 +1,31 @@
-const app = require('app')
+const app = require('./app')
+console.log('debug')
+console.log(app)
+
+app.event("app_mention", async ({ event, context, client, say }) => {
+  try {
+    await say({
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `Thanks for the mention <@${event.user}>! Here's a button`
+          },
+          accessory: {
+            type: "button",
+            text: {
+              type: "plain_text",
+              text: "Button",
+              emoji: true
+            },
+            value: "click_me_123",
+            action_id: "first_button"
+          }
+        }
+      ]
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
