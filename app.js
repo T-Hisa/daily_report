c832661eb7ab729d6c8cfe11112fb1a80963514a
Glitch (hello-express) <none>
2021-11-28 23:08:28 +0000

    ⏰🚋 Checkpoint ./app.js:39260817/1511

--- app.js
+++ app.js
@@ -10,8 +10,67 @@ module.exports = app
 
 require('./actions')
 require('./events')
-require('./commands')
-
+// require('./commands')
+app.command('/hello_world', async ({ ack, payload, context }) => {
+  // Acknowledge the command request
+  console.log('received!!!!!!!!!!!!!!!')
+  ack();
+  try {
+    const result = await app.client.views.open({
+      token: context.botToken,
+      // Pass a valid trigger_id within 3 seconds of receiving it
+      trigger_id: payload.trigger_id,
+      // View payload
+      view: {
+        type: 'modal',
+        // View identifier
+        callback_id: 'view_1',
+        title: {
+          type: 'plain_text',
+          text: 'Modal title'
+        },
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: 'Welcome to a modal with _blocks_'
+            },
+            accessory: {
+              type: 'button',
+              text: {
+                type: 'plain_text',
+                text: 'Click me!'
+              },
+              action_id: 'button_abc'
+            }
+          },
+          {
+            type: 'input',
+            block_id: 'test_input',
+            label: {
+              type: 'plain_text',
+              text: 'What are your hopes and dreams?'
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'dreamy_input',
+              multiline: true
+            }
+          }
+        ],
+        submit: {
+          type: 'plain_text',
+          text: 'Submit'
+        }
+      }
+    });
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
 // All the room in the world for your code
 const start_app = async () => {
   await app.start(process.env.PORT || 3000);
