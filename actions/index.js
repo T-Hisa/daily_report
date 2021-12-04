a4d079bcabdb74f983e8d7cf2abbed4deb8b488b
Glitch (hello-express) <none>
2021-12-04 14:34:39 +0900

    Clean up around action

--- actions/index.js
+++ actions/index.js
@@ -1,3 +1,2 @@
 require('./button_abc')
 require('./first_button')
-require('./plain_text_input_action')
--- actions/plain_text_input_action.js
+++
@@ -1,7 +0,0 @@
-const app = require('../app')
-
-app.action('plain_text_input_action', async ({ ack, body, context }) => {
-
-  ack();
-  console.log('plain_text was input !!!!!!!!!')
-})
