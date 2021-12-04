247a0fe0ec74f89423ca358362128a1f436283d8
Glitch (hello-express) <none>
2021-12-05 08:49:52 +0900

    Define global level variable of element_count

--- actions/removing-action.js
+++ actions/removing-action.js
@@ -2,21 +2,19 @@ const app = require("../app");
 const { plain_text_field_generator } = require("../elements");
 const { base_modal_view } = require("../views");
 
-const MAX_ELEMENT_COUNT = 5
-let HIGH_LEVEL_ACTION_ELEMENT_COUNT = 1;
+let { HIGH_LEVEL_ACTION_ELEMENT_COUNT } = require("./element_count");
+
+const MIN_ELEMENT_COUNT = 5;
 
 const findElementByActionId = (blocks, priority) => {
   // "elements" 要素があり、かつ、その "eleements"要素の "action_id" 要素が actionId に等しい
   // 配列の要素の Index を返す。
   return blocks.indexOf(
     blocks.find(
-      (block) =>
-        block["elements"] &&
-        block["elements"][1]["value"] === priority
+      (block) => block["elements"] && block["elements"][1]["value"] === priority
     )
   );
 };
-
 
 const addingElementToBlocks = (blocks, priority) => {
   const addingIndex = findElementByActionId(blocks, priority);
@@ -38,7 +36,7 @@ app.action("adding-action", async ({ ack, body, context }) => {
   console.log(body["actions"][0]["value"]);
   const base_blocks = base_modal_view["blocks"];
   if (HIGH_LEVEL_ACTION_ELEMENT_COUNT < MAX_ELEMENT_COUNT) {
-    addingElementToBlocks(base_blocks, 'high')
+    addingElementToBlocks(base_blocks, "high");
   }
   // body['view']['blocks'] に、要素を追加する。
   try {
