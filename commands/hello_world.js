86f74151618d9454ff8516c918b15ea13856e555
Glitch (hello-express) <none>
2021-12-05 09:16:17 +0900

    Change

--- commands/hello_world.js
+++ commands/hello_world.js
@@ -1,6 +1,5 @@
 const app = require("../app");
 const { base_modal_view } = require("../views");
-const { button_generator, plain_text_field_generator } = require("../elements");
 
 // Listen for a slash command invocation
 app.command("/hello_world", async ({ ack, payload, context }) => {
@@ -8,14 +7,13 @@ app.command("/hello_world", async ({ ack, payload, context }) => {
   ack();
   console.log('command invoked !!!')
   try {
-    const result = await app.client.views.open({
+    await app.client.views.open({
       token: context.botToken,
       // Pass a valid trigger_id within 3 seconds of receiving it
       trigger_id: payload.trigger_id,
       // View payload
       view: base_modal_view,
     });
-    // console.log(result);
   } catch (error) {
     console.error(error);
   }
